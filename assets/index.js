47e9f039248e7c3d89e867e2872bfac0dc68fb9b
mukukuu <qinii@me.com>
2022-04-05 21:49:43 -0700

    create function to show weather on card

--- assets/index.js
+++ assets/index.js
@@ -10,7 +10,7 @@ var citySearchButton = $("#search-button");
 
 
 
-//show current weather
+//----------------------show current weather
 function displayWeather(event) {
     event.preventDefault();
     if (citySearch.val().trim() !== "") {
@@ -42,4 +42,56 @@ function displayWeather(event) {
       }
     }
   }
+  
+
+
+  //----------------------------function to display weather in weather card
+   function currentWeather(city) {
+    const apiKey = "ee41023e27cbe7d2955c0ddebe7d0f31";
+    var queryUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=" + apiKey + "&units=imperial";
+  
+    $.ajax({
+      url: queryUrl,
+      method: "GET",
+    }).then(function (weatherData) {
+  
+      var weathericon = weatherData.weather[0].icon;
+      var iconurl = "https://openweathermap.org/img/wn/" + weathericon + "@2x.png";
+      var now = dayjs();
+      var city = document.getElementById("current-city");
+      city.innerHTML = (weatherData.name + " " + "(" + now.format("MM/DD/YYYY") + ")" + '<img src="' + iconurl + '">');
+  
+      var temp = document.getElementById("temperature");
+      temp.textContent = "Temperature: " + weatherData.main.temp + " °F";
+  
+      var humidity = document.getElementById("humidity");
+      humidity.textContent = "Humidity: " + weatherData.main.humidity + "%";
+  
+      var wind = document.getElementById("wind-speed");
+      wind.textContent = "Wind Speed: " + weatherData.wind.speed + " MPH";
+  
+      //--------------------- gets UV data, sets icon color based on number retrieved
+      var latValue = weatherData.coord.lat;
+      var lonValue = weatherData.coord.lon;
+      var queryUv = "https://api.openweathermap.org/data/2.5/uvi?";
+      var uvdata = queryUv + "lat=" + latValue + "&lon=" + lonValue + "&appid=" + apiKey
+  
+      $.ajax({
+        url: uvdata,
+        method: "GET",
+      }).then(function (uvIndexData) {
+        var uvIndex = document.getElementById("uv-index");
+        uvIndex.textContent = "UV Index: " + uvIndexData.value;
+  
+        var uvText = uvIndexData.value;
+        if (uvText <= 2) {
+          uvIndex.setAttribute("class", "badge bg-success");
+        }
+        else if (uvText <= 6) {
+          uvIndex.setAttribute("class", "badge bg-warning");
+        }
+        else if (uvText > 6) {
+          uvIndex.setAttribute("class", "badge bg-danger");
+        }
+      });
   
